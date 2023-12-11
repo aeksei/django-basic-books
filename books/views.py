c9fd150b191ca6ee6fa6ffabec0dfb177446780a
aeksei <aeksei94@gmail.com>
2023-12-12 00:07:28 +0500

    add view with books list and detail book

--- books/views.py
+++ books/views.py
@@ -28,7 +28,12 @@ def about_handler(request):
     return HttpResponse(f"Сайт разработан в {now.year}.")
 
 
-def books_list(request):
+def books_list(request: HttpRequest):
+    query_author = request.GET.get("author")
+    if query_author:
+        books_list = [book for book in books if book['author'] == query_author]
+        return JsonResponse(books_list, safe=False, json_dumps_params={"ensure_ascii": False, "indent": 4})
+
     return JsonResponse(books, safe=False, json_dumps_params={"ensure_ascii": False, "indent": 4})
 
 
@@ -41,3 +46,14 @@ def randon_book_with_missing(request):
     id_ = randint(0, 10)
     book = get_object_or_404(books, id_)
     return JsonResponse(book, json_dumps_params={"ensure_ascii": False, "indent": 4})
+
+
+def get_path(request, sub_path_1):
+    return HttpResponse(f"значение")
+
+
+def get_book_detail(request, book_id: int):
+    return JsonResponse(
+        get_object_or_404(books, book_id),
+        json_dumps_params={"ensure_ascii": False, "indent": 4}
+    )
